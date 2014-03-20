50ec8a173dbf80523dd8fb0863530f94e63e2270
aaron1978 <klawdragon@gmail.com>
2014-03-20 01:44:30 +0100

    Update pwncrypt5.py

--- pwncrypt5.py
+++ pwncrypt5.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python              
+python pwncrypt5.py silvanya82@gmail.com msgstore.db.crypt5 > msgstore.sdb
 """
 48bits presents:
 8===============================================D~~~
@@ -12,8 +12,10 @@ import hashlib
 import StringIO
 from M2Crypto import EVP
  
-key = bytearray([141, 75, 21, 92, 201, 255, 129, 229, 203, 246, 250, 120, 25, 54, 106, 62, 198, 33, 166, 86, 65, 108, 215, 147])
-iv = bytearray([0x1E,0x39,0xF3,0x69,0xE9,0xD,0xB3,0x3A,0xA7,0x3B,0x44,0x2B,0xBB,0xB6,0xB0,0xB9])
+key = bytearray([141, 75, 21, 92, 201, 255, 129, 229, 203, 246, 250, 
+   120, 25, 54, 106, 62, 198, 33, 166, 86, 65, 108, 215, 147])
+iv = bytearray([0x1E,0x39,0xF3,0x69,0xE9,0xD,0xB3,0x3A,0xA7,0x3B,0x44,
+   0x2B,0xBB,0xB6,0xB0,0xB9])
  
 def decrypt(db,acc):
   fh = file(db,'rb')
@@ -29,7 +31,8 @@ def decrypt(db,acc):
  
 if __name__ == '__main__':
   if len(sys.argv) != 3:
-    print 'usage %s <db> <accountname> > decrypted.db' % sys.argv[0]
+    print 'usage %s <db> <silvanya82@gmail.com> > decrypted.db' % sys.argv[0]
   else:
     decrypt(sys.argv[1],sys.argv[2])
+
 
